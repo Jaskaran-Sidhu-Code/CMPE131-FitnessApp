1a7658e1928545466f8b4a8534b3d52b503377e8
ecforney <161394654+ecforney@users.noreply.github.com>
2024-04-29 15:56:37 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, g, render_template, request, make_response
 import sqlite3
 
-userID = "Erich"
-
 app = Flask(__name__)
 
 def connectDB(dbName):
-    sql = sqlite3.connect('./database.db')
+    sql = sqlite3.connect(dbName)
     sql.row_factory = sqlite3.Row
     return sql
 
@@ -20,9 +18,38 @@ def closeDB(error):
     if hasattr(g,'sqlite3_db'):
         g.sqlite3_db.close()
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
+def login():
+    db = getDB('./loginDatabase.db')
+    current_user = request.form.get('username')
+    password = request.form.get('password')
+    connection = sqlite3.connect('loginDatabase.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT password FROM Login WHERE username=? and password=?",[current_user, password])
+    pw = cursor.fetchone()
+    connection.close()
+    global currentUser
+    currentUser = current_user
+    try: 
+        if pw[0] == password:
+            return render_template("homePage.html",usr=currentUser)
+    except:
+        return render_template("login.html")
+
+@app.route('/create_account', methods=["GET","POST"])
+def create_account():
+    if request.method == "POST":
+        db = getDB('./loginDatabase.db')
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        db.execute('INSERT INTO Login (username, email, password) VALUES (?,?,?);',[username,email,password])
+        db.commit()
+    return render_template("accountCreation.html")
+
+@app.route('/home')
 def homePage():
-    user = showGoals(userID)
+    user = showGoals()
     return render_template("homePage.html", usr = user)
 
 @app.route('/goalselect', methods = ["GET","POST"])
@@ -30,27 +57,26 @@ def goalSelect():
     if request.method == "POST":
         print(request.form)
         db = getDB('./database.db')
-        user = request.form.get("user",False)
         goalOne = request.form.get("goalOne",False)
         goalTwo = request.form.get("goalTwo",False)
         goalThree = request.form.get("goalThree",False)
         goalFour = request.form.get("goalFour",False)
         goalFive = request.form.get("goalFive",False)
-        db.execute("INSERT OR REPLACE INTO goalselect (user,goalOne,goalTwo,goalThree,goalFour,goalFive) VALUES (?,?,?,?,?,?);", [userID,goalOne,goalTwo,goalThree,goalFour,goalFive])
+        db.execute("INSERT OR REPLACE INTO goalselect (user,goalOne,goalTwo,goalThree,goalFour,goalFive) VALUES (?,?,?,?,?,?);", [currentUser,goalOne,goalTwo,goalThree,goalFour,goalFive])
         db.commit()
-    return render_template("goalSelect.html")
+    return render_template("goalSelect.html",usr=currentUser)
 
-def showGoals(user):
+def showGoals():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM goalselect WHERE user=?",[user])
+    cursor.execute("SELECT * FROM goalselect WHERE user=?",[currentUser])
     results = cursor.fetchone()
     connection.close()
     return results
 
 @app.route('/calorietracker')
 def calorieTracker():
-    return render_template("calorieTracker.html")
+    return render_template("calorieTracker.html",usr=currentUser)
 
 @app.route("/temp")
 def temp():
